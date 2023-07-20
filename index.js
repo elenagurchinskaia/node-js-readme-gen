3b719d971c93eece31bb2130345bf3a6021726b1
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-07-20 17:42:55 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// TODO: Include packages needed for this application
+const readme = require("generateMarkdown");
+// add a package that would import generate file
+// export from "generateMarkdown"
+// to import we will use require key word  ex: const badmath = require('./generateMarkdown.js');
+// > use your own word to name the variable (not matching the 'generateMarkdown')
+// node index.js will be executed
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  // create the readme
+}
+// TODO: Create a function to initialize app
+function init() {
+  // ask the user questions - array
+  // store answers as one {}
+  // create a content for the readme (function) generate markdown
+  // > calling generate markdown function
+  // > insert ojects of answers
+  // invoke the writeToFile (which created the readme)
+  console.log("Hello");
+}
+
+// Function call to initialize app
+//
+init();
+
+// var result = init();
