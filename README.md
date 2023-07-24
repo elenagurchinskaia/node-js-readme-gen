81d9b2bfd0a49d4c166beebe5d7a258b4241e842
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-07-24 10:43:00 -0500

    updated renderLicenseSection

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# node-js-readme-gen
+# sdhkjad
+
+
+
+asdhvk
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Questions](#questions)
+
+## Installation 
+To install the required dependencies, run the following command:
+
+```
+npm i
+```
+
+## Usage
+skdvnk
+
+To run tests, use the following commands:
+
+```
+npm test
+```
+
+## License
+This project is licensed under the NONE License
+
+
+## Contributing
+asldvnl
+
+
+## Questions
+For any questions or inquires, feel free to reach out to me via GitHub:
+[ksdhvka](https://github.com/ksdhvka) or Email: ihdvka.
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,4 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === "Apache 2.0") {
     return "https://img.shields.io/badge/License-Apache_2.0-blue.svg"; // ![License]
@@ -9,13 +8,12 @@ function renderLicenseBadge(license) {
     return "https://img.shields.io/badge/License-GPLv3-blue.svg"; // ![License: GPL v3]
   } else if (license === "BSD 3") {
     return "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg"; // ![License]
-  } else if (license === "NONE") {
-    return "";
+    // } else if (license === "NONE") {
+    //   return "";
   }
 }
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === "Apache 2.0") {
     return "https://opensource.org/licenses/Apache-2.0"; // ![License]
@@ -25,14 +23,21 @@ function renderLicenseLink(license) {
     return "https://www.gnu.org/licenses/gpl-3.0"; // ![License: GPL v3]
   } else if (license === "BSD 3") {
     return "https://opensource.org/licenses/BSD-3-Clause"; // ![License]
-  } else if (license === "NONE") {
-    return "";
+    // } else if (license === "NONE") {
+    //   return "";
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "NONE") {
+    return "";
+  }
+  return `[![License](${renderLicenseBadge(license)})](${renderLicenseLink(
+    license
+  )})`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -40,46 +45,48 @@ function generateMarkdown(data) {
   const licenseBadge = renderLicenseBadge(data.license);
   // const licenseLink = renderLicenseLink(data.license);
   // create the README content using the user's input data
-  return `
-  #${data.appName}
-  ${data.appDescription}
+  return `# ${data.appName}
 
-  ## Table of Contents
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [License](#license)
-  - [Contributing](#contributing)
-  - [Questions](#questions)
+${renderLicenseSection(data.license)}
 
-  ## Installation 
-  To install the required dependencies, run the following command:
+${data.appDescription}
 
-  \`\`\`
-  ${data.commandDep}
-  \`\`\`
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Questions](#questions)
 
-  ## Usage
-  ${data.repoInfo}
-  
-  To run tests, use the following commands:
+## Installation 
+To install the required dependencies, run the following command:
 
-  \`\`\`
-  ${data.commandTest}
-  \`\`\`
-  
-  ## License
-  This project is licensed under the ${data.license} License
+\`\`\`
+${data.commandDep}
+\`\`\`
 
+## Usage
+${data.repoInfo}
 
-  ## Contributing
-  ${data.repoUsage}
+To run tests, use the following commands:
+
+\`\`\`
+${data.commandTest}
+\`\`\`
+
+## License
+This project is licensed under the ${data.license} License
 
 
-  ## Questions
-  For any questions or inquires, feel free to reach out to me via GitHub:
-  [${data.github}](https://github.com/${data.github}) or Email: ${data.email}.
- 
-  `;
+## Contributing
+${data.repoUsage}
+
+
+## Questions
+For any questions or inquires, feel free to reach out to me via GitHub:
+[${data.github}](https://github.com/${data.github}) or Email: ${data.email}.
+
+`;
 }
 
 // inject the stuff (can use markdown language)
