2f882f9372ab512228a52283d8736ed23f872ee8
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-07-23 20:12:22 -0500

    added questions and licenses

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
 // TODO: Include packages needed for this application
-const readme = require("generateMarkdown");
-// add a package that would import generate file
-// export from "generateMarkdown"
+const inquirer = require("inquirer");
+const fs = require("fs");
+const readmeText = require("./utils/generateMarkdown");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // to import we will use require key word  ex: const badmath = require('./generateMarkdown.js');
 // > use your own word to name the variable (not matching the 'generateMarkdown')
 // node index.js will be executed
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your gitHub repo username?",
+    name: "github",
+  },
+
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data);
   // create the readme
 }
+
 // TODO: Create a function to initialize app
 function init() {
   // ask the user questions - array
+  inquirer.prompt(questions).then((data) => {
+    // answers
+    console.log(data);
+  });
+  const markdown = generateMarkdown(data);
+  writeToFile("README.md", markdown)
+    // create content from the answers
+    // call writeToFile
+    .catch((error) => {
+      console.log(error);
+    });
   // store answers as one {}
   // create a content for the readme (function) generate markdown
   // > calling generate markdown function
   // > insert ojects of answers
   // invoke the writeToFile (which created the readme)
-  console.log("Hello");
+  //   console.log("Hello");
 }
 
 // Function call to initialize app
 //
 init();
-
+writeToFile();
+// console.log("Hello world");
 // var result = init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "Apache 2.0") {
+    return "https://img.shields.io/badge/License-Apache_2.0-blue.svg"; // ![License]
+  } else if (license === "MIT") {
+    return "https://img.shields.io/badge/License-MIT-yellow.svg"; // ![License: MIT]
+  } else if (license === "GPL 3.0") {
+    return "https://img.shields.io/badge/License-GPLv3-blue.svg"; // ![License: GPL v3]
+  } else if (license === "BSD 3") {
+    return "https://img.shields.io/badge/License-BSD_3--Clause-blue.svg"; // ![License]
+  } else if (license === "NONE") {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,12 +24,19 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-// inject the stuff (can use markdown language)
-// 
-`;
+  // generate the license badge and link
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseBadge(data.license);
+  // create the README content using the user's input data
+  return `
+  #${data.appName}
+  ${data.appDescription}
+
+  
 }
 
+// inject the stuff (can use markdown language)
+//
 module.exports = generateMarkdown;
 
-// work on index.js 30% of the time and license 70%
+renderLicenseBadge();
